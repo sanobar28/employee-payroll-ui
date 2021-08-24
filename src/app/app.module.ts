7a449dc48e5c47d8e9a1ce0626a716e8bcd6452a
sanobar28 <sanobar.mujawar@gmail.com>
2021-08-24 08:11:17 +0530

    added: header component and assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AddComponent } from './component/add/add.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
