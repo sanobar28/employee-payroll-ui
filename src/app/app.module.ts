8a0216b999e4fb9381025615cf8388e6b88ca9f9
sanobar28 <sanobar.mujawar@gmail.com>
2021-08-25 16:12:42 +0530

    added: home component, service class and minor changes in css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import { MatSliderModule } from '@angular/material/slider'; 
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatRadioModule,
     MatDatepickerModule,
     MatButtonModule,
+    HttpClientModule,
     MatSliderModule
   ],
   providers: [],
---
+++ src/app/service/http.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(
+    private httpClient : HttpClient) {
+   
+  }
+  
+  getEmployeeData() : Observable<any> {
+    return this.httpClient.get("http://localhost:8081/employeepayrollservice/get");
+  }
+
+}
