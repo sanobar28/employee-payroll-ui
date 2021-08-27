c99dac659bc30bc5c31ad784e301eee726534160
sanobar28 <sanobar.mujawar@gmail.com>
2021-08-27 09:18:30 +0530

    refactor: segregated angular material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,10 @@ import { HeaderComponent } from './component/header/header.component';
 import { AddComponent } from './component/add/add.component';
 import { HomeComponent } from './component/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import { MatSliderModule } from '@angular/material/slider'; 
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatButtonModule} from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularMaterialModule } from './material/angular-material/angular-material.module';
+
+
 
 @NgModule({
   declarations: [
@@ -27,14 +23,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatDatepickerModule,
-    MatButtonModule,
-    HttpClientModule,
-    MatSliderModule
+    AngularMaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
